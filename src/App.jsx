f149a1606bc5b9270235f83685198d19de62511e
Lyss2120 <lys.ivonne@gmail.com>
2025-12-30 13:17:56 -0300

    shuffledCards fn added

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,38 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
+const cardImages = [
+  {"src": "img/helmet-1.png", "matched": false},
+  {"src": "img/potion-1.png", "matched": false},      
+  {"src": "img/ring-1.png", "matched": false},
+  {"src": "img/scroll-1.png", "matched": false},
+  {"src": "img/shield-1.png", "matched": false},
+  {"src": "img/sword-1.png", "matched": false}  
+]
+
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [cards, setCards] = useState([])
+  const [turns, setTurns] = useState(0)
+
+  const shuffledCards = () => {
+    const shuffledCards = [...cardImages, ...cardImages]
+      .sort(()=> Math.random() - 0.5)
+      .map((card)=>({...card, id: Math.floor(Math.random() * 12) + 1 }))
+
+      setCards(shuffledCards)
+      setTurns(0)
+  }
+
+console.log(cards);
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className='App'> 
+        <h1>Magic Memory</h1>
+        <button>New Game</button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
